e0c28727d80b3e563bbc2751b2b636839b18a2d0
FaithSlade <faithwslade@gmail.com>
2022-12-22 20:32:25 -0800

    add code to teacher class

--- src/Task/Teacher.java
+++ src/Task/Teacher.java
@@ -3,5 +3,6 @@ package Task;
 public class Teacher {
     public static void main(String[] args) {
         System.out.println("Faith created this class ");
+        System.out.println("I try to merge one more time");
     }
 }
