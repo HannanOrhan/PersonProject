3258a3f536153f8a2335a0c290b3f3673369affe
Hakob <hakobv68@gmail.com>
2022-12-22 20:06:39 -0800

    Employee class created

--- src/Task/Employee.java
+++ src/Task/Employee.java
@@ -1,5 +1,29 @@
 package Task;
 
 public class Employee {
+
+    private int year;
+    private double price;
     //created
+
+    public Employee(int year, double price) {
+        setYear(year);
+        setPrice(year);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
